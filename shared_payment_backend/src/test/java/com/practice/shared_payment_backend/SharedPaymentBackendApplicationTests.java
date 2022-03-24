a585559d3bde207829879c55a24f1111ed65f1fd
Daniel Alderete <daniel.alderetecillero@gmail.com>
2022-03-24 09:57:27 +0100

    Started Spring project

---
+++ shared_payment_backend/src/test/java/com/practice/shared_payment_backend/SharedPaymentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.shared_payment_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedPaymentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
